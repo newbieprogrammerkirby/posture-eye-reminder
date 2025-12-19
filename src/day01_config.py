1909bf7dad945e71b8d2ac0169c9894ee3ff5d20
Kirby <kirbysolana13@gmail.com>
2025-12-20 01:33:58 +0800

    Day 2 - Learned loops, not sure how to integrate it to my project yet. Fixed spelling issues

--- src/day01_config.py
+++ src/day01_config.py
@@ -1,11 +1,16 @@
 work_minutes = int(input("Minutes worked so far: "))
+water = input("")
 
 eyer_alarm_time = 20
+water_alarm_time = 30
 posture_alarm_time = 60
 
+
 if work_minutes >= posture_alarm_time:
-    print("ALARM: Be concious of your neck posture")
+    print("ALARM: Be conscious of your neck posture")
 elif work_minutes >= eyer_alarm_time:
     print("ALARM: Look away from the computer and slow blinks")
+elif work_minutes >= water_alarm_time:
+    print("ALARM: Hydrate")
 else:
     print("No alarm yet.")
